6fc860aee69bb49965be3fade8aae9d402a1b637
Pablo Martínez López <pmartinezl@full-on-net.com>
2022-05-13 12:59:57 +0200

    Initial Commit

---
+++ SwiftUITransitions/SwiftUITransitionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITransitionsApp.swift
+//  SwiftUITransitions
+//
+//  Created by Pablo Martinez Lopez on 13/5/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITransitionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
